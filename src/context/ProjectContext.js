39fda47681f33cfe724e8794c2f70c6c4a3b9d58
abelHodasz <hodabi@gmail.com>
2020-03-04 22:36:45 +0100

    Project Context with sample data

--- src/context/ProjectContext.js
+++ src/context/ProjectContext.js
@@ -1,24 +1,61 @@
 import React, { useState, createContext } from "react";
+import sortingImage from "../static/images/sorting-abelHodasz.png";
+import sortingImageMobile from "../static/images/sorting-abelHodasz-mobile.png";
+import sortingLogo from "../static/icons/sorting-abelHodasz.png";
+export const ProjectContext = createContext();
 
-export const ProjectProvider = createContext();
-
-export default function ProjectContext(props) {
+export const ProjectProvider = props => {
     const projects = [
         {
+            name: "Sorting Algorithms",
+            id: "sorting-algorithms",
             featured: true,
             link: "https://sorting-hodabi.netlify.com/",
             gitHubLink: "https://github.com/CodecoolGlobal/sorting-abelHodasz",
-            logo: "static/logos/sortin-abelHodasz.png",
-            image: "static/images/sortin-abelHodasz.png"
+            page: "/projects/sorting-algorithms",
+            logo: sortingLogo,
+            image: sortingImage,
+            imageMobile: sortingImageMobile
+        },
+        {
+            name: "Sorting Algorithms",
+            id: 1,
+            featured: true,
+            link: "https://sorting-hodabi.netlify.com/",
+            gitHubLink: "https://github.com/CodecoolGlobal/sorting-abelHodasz",
+            page: "/projects/sorting-algorithms",
+            logo: sortingLogo,
+            image: sortingImage,
+            imageMobile: sortingImageMobile
+        },
+        {
+            name: "Sorting Algorithms",
+            id: 2,
+            featured: true,
+            link: "https://sorting-hodabi.netlify.com/",
+            gitHubLink: "https://github.com/CodecoolGlobal/sorting-abelHodasz",
+            page: "/projects/sorting-algorithms",
+            logo: sortingLogo,
+            image: sortingImage,
+            imageMobile: sortingImageMobile
+        },
+        {
+            name: "Sorting Algorithms",
+            id: 3,
+            featured: true,
+            link: "https://sorting-hodabi.netlify.com/",
+            gitHubLink: "https://github.com/CodecoolGlobal/sorting-abelHodasz",
+            page: "/projects/sorting-algorithms",
+            logo: sortingLogo,
+            image: sortingImage,
+            imageMobile: sortingImageMobile
         }
     ];
-    const [state, setState] = useState({
-        ...projects
-    });
+    const [state, setState] = useState(projects);
 
     return (
         <ProjectContext.Provider value={[state, setState]}>
             {props.children}
         </ProjectContext.Provider>
     );
-}
+};
