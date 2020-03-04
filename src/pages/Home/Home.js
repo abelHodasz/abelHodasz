716552afcd801bee666a1b358c4a9893451a4cc7
abelHodasz <hodabi@gmail.com>
2020-03-04 22:37:14 +0100

    Remove Projects from home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./Home.css";
-import ProjectSlider from "../../components/ProjectSlider/ProjectSlider";
 export default function Home() {
     return (
         <div className="home">
-            <h1>Ábel Hodász</h1>
-            <div className="projects-container">
-                <ProjectSlider />
-            </div>
+            <h1 className="title">Ábel Hodász</h1>
         </div>
     );
 }
