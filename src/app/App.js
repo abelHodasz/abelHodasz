45cec0bc0662f689310d91a5398b45d7bd205344
abelHodasz <hodabi@gmail.com>
2020-03-31 14:48:26 +0200

    Remove background

--- src/app/App.js
+++ src/app/App.js
@@ -7,10 +7,11 @@ import AboutMe from "../pages/AboutMe/AboutMe";
 import Project from "../pages/Project/Project";
 import ProjectsByCategory from "../pages/ProjectsByCategory/ProjectsByCategory";
 import Projects from "../pages/Projects/Projects";
-import { Container, ThemeProvider } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core";
 import { basic } from "../themes/themes";
 import { ProjectProvider } from "../context/ProjectContext";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Background from "../components/Background/Background";
 import topBackground from "../static/svg/top.svg";
 import middleBackground from "../static/svg/middle.svg";
 import bottomBackground from "../static/svg/bottom.svg";
@@ -18,52 +19,12 @@ import anime from "animejs/lib/anime.es.js";
 
 function App() {
     const content = <Content />;
-    const [scrollTop, setScrollTop] = useState(
-        document.querySelector("html").scrollTop
-    );
-    useEffect(() => {
-        window.addEventListener("scroll", e => {
-            const html = document.querySelector("html");
-            const scrollTop = html.scrollTop;
-            setScrollTop(scrollTop);
-        });
-    }, []);
 
-    const topStyle = { transform: "translateY(-" + scrollTop + "px)" };
-
-    const middleStyle = {
-        transition: "all 1.5s ease",
-        transform: "scale(1.02) translateX(1%) translateY(-" + scrollTop + "px)"
-    };
-
-    const bottomStyle = {
-        transition: "transform 3s ease",
-        transform:
-            "scale(1.04) translateX(-1%) translateY(-" + scrollTop + "px)"
-    };
+    useEffect(() => {}, []);
 
     return (
         <ProjectProvider>
-            <div className="background-container">
-                <img
-                    style={topStyle}
-                    className="top-background background-svg"
-                    src={topBackground}
-                    alt=""
-                />
-                <img
-                    style={middleStyle}
-                    className="middle-background background-svg"
-                    src={middleBackground}
-                    alt=""
-                />
-                <img
-                    style={bottomStyle}
-                    className="bottom-background background-svg"
-                    src={bottomBackground}
-                    alt=""
-                />
-            </div>
+            {/*<Background />*/}
             {content}
         </ProjectProvider>
     );
