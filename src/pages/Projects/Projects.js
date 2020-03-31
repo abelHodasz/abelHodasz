5951784fdbc70c781361d61f9a3be6a9e81a7265
abelHodasz <hodabi@gmail.com>
2020-03-31 14:48:35 +0200

    Formatting

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -11,7 +11,7 @@ export default function Projects() {
 
     return (
         <div>
-                <h1 className="title">Projects</h1>
+            <h1 className="title">Projects</h1>
             <span className="link-container">
                 <span className="link back" onClick={() => history.goBack()}>
                     <BackIcon />
