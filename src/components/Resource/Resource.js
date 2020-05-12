893e009db80072d7761ecc7e9dc1ae63e2979457
abelHodasz <49756494+abelHodasz@users.noreply.github.com>
2020-05-12 17:42:49 +0200

    Added resources page

---
+++ src/components/Resource/Resource.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Resource.css";
+
+export default function Resource(props) {
+    return (
+        <div className="resource">
+            <div>
+                <img
+                    className="resource-img"
+                    height="40"
+                    src={props.img}
+                    alt={props.name}
+                ></img>
+            </div>
+            <a className="resource-link" href={props.link}>
+                {props.name}
+            </a>
+            <p>{props.children}</p>
+        </div>
+    );
+}
